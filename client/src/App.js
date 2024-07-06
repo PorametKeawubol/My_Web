8ddafdb64bc27233974f98f40299a7ef2ee073e9
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-07-06 19:58:02 +0700

    add tailwind css in react

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My App</h1>
+        <h1 className="text-3xl font-bold ">Welcome to My App</h1>
         <ItemList />
       </header>
     </div>
